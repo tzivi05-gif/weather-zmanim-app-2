a8ac416fe8488af5def3ad273f2bc5cc1ba3af41
tzivi05-gif <tzivi05@gmail.com>
2026-01-27 18:17:20 -0500

    added npm install

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,9 @@
 const { execSync } = require("child_process");
 
+if (!process.env.VERCEL) {
+  execSync("npm --prefix backend install", { stdio: "inherit" });
+}
+
 const command = process.env.VERCEL
   ? "react-scripts build"
   : "npm run build:server && react-scripts build";
